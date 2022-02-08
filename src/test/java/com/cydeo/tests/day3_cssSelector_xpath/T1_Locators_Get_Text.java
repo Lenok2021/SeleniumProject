3760967d7469383dc3b18bc1a9b5a43034c894f2
cross <crosstown11223@yahoo.com>
2022-02-08 11:30:12 -0500

    set up WebDriver

--- src/test/java/com/cydeo/tests/day3_cssSelector_xpath/T1_Locators_Get_Text.java
+++ src/test/java/com/cydeo/tests/day3_cssSelector_xpath/T1_Locators_Get_Text.java
@@ -1,7 +1,9 @@
 package com.cydeo.tests.day3_cssSelector_xpath;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class T1_Locators_Get_Text {
@@ -11,8 +13,10 @@ public class T1_Locators_Get_Text {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
 
-        driver.get("https://login1>nextbaserm.com/");
+        driver.get("https://login1.nextbasecrm.com/");
 
+         // enter incorrect username: "incorrect"
+        WebElement login = driver.findElement(By.className("login-inp"));
 
 
 
