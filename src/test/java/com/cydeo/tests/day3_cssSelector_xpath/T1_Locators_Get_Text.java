2383e8de330e420ad6e8c7c03e5703b104d5ae38
cross <crosstown11223@yahoo.com>
2022-02-08 11:24:48 -0500

    set up WebDriver

---
+++ src/test/java/com/cydeo/tests/day3_cssSelector_xpath/T1_Locators_Get_Text.java
@@ -0,0 +1,29 @@
+package com.cydeo.tests.day3_cssSelector_xpath;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class T1_Locators_Get_Text {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://login1>nextbaserm.com/");
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
