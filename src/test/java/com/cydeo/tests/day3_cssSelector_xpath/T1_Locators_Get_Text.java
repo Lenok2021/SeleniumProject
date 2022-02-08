79bd34adf05e645c42544d5fd31b29008f9cbe05
cross <crosstown11223@yahoo.com>
2022-02-08 12:36:26 -0500

    set up WebDriver

--- src/test/java/com/cydeo/tests/day3_cssSelector_xpath/T1_Locators_Get_Text.java
+++ src/test/java/com/cydeo/tests/day3_cssSelector_xpath/T1_Locators_Get_Text.java
@@ -1,5 +1,6 @@
 package com.cydeo.tests.day3_cssSelector_xpath;
 
+import com.cydeo.utilities.WebDriverFactory;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -12,9 +13,11 @@ public class T1_Locators_Get_Text {
 
         //TC #1: NextBaseCRM, locators and getText() practice
         //1- Open a Chrome browser
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
+       // WebDriverManager.chromedriver().setup();
+      //  WebDriver driver = new ChromeDriver();
+       // driver.manage().window().maximize();
+
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
 
         //2- Go to: https://login1.nextbasecrm.com/
         driver.get("https://login1.nextbasecrm.com/");
--- src/test/java/com/cydeo/utilities/WebDriverFactory.java
+++ src/test/java/com/cydeo/utilities/WebDriverFactory.java
@@ -3,13 +3,15 @@ package com.cydeo.utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 public class WebDriverFactory {
 
     public static void main(String[] args) {
 
     }
-        // TASK: NEW METHOD CREATION
+    // TASK: NEW METHOD CREATION
 // Method name : getDriver
 // Static method
 // Accepts String arg: browserType
@@ -19,6 +21,27 @@ public class WebDriverFactory {
 // RETURN TYPE: "WebDriver"
 
 
+    public static WebDriver getDriver(String browserType) {
+
+        WebDriver driver = null;
+
+        if (browserType.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+
+        } else if (browserType.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+            driver.manage().window().maximize();
+
+        }else{
+            System.out.println("Invalid driver");
+        }
+        return driver;
 
     }
+
+
+}
 
