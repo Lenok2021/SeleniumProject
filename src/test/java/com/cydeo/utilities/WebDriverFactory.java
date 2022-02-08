6e61aa22342e45df5ad81d80aa00324ca92569d8
cross <crosstown11223@yahoo.com>
2022-02-08 11:53:40 -0500

    set up WebDriver

---
+++ src/test/java/com/cydeo/utilities/WebDriverFactory.java
@@ -0,0 +1,26 @@
+package com.cydeo.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebDriverFactory {
+
+    public static void main(String[] args) {
+
+        // TASK: NEW METHOD CREATION
+// Method name : getDriver
+// Static method
+// Accepts String arg: browserType
+//   - This arg will determine what type of browser is opened
+//   - if "chrome" passed --> it will open chrome browser
+//   - if "firefox" passed --> it will open firefox browser
+// RETURN TYPE: "WebDriver"
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        
+
+    }
+}
