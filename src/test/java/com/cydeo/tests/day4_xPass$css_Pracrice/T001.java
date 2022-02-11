a92c613aac4e226ac94e64c98c5505157b534620
cross <crosstown11223@yahoo.com>
2022-02-11 11:28:27 -0500

    created method browser

---
+++ src/test/java/com/cydeo/tests/day4_xPass$css_Pracrice/T001.java
@@ -0,0 +1,44 @@
+package com.cydeo.tests.day4_xPass$css_Pracrice;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class T001 {
+    public static void main(String[] args) {
+
+      /*
+      DO NOT USE ANY DEVELOPER TOOLS TO GET ANY LOCATORS.
+TC #1: XPATH and cssSelector Practices
+1. Open Chrome browser
+2. Go to https://practice.cydeo.com/forgot_password
+3. Locate all the WebElements on the page using XPATH and/or CSS
+locator only (total of 6)
+a. “Home” link
+b. “Forgot password” header
+c. “E-mail” text
+d. E-mail input box
+e. “Retrieve password” button
+f. “Powered by Cydeo text
+4. Verify all web elements are displayed.
+First solve the task with using cssSelector only. Try to create 2 different
+cssSelector if possible
+
+Then solve the task using XPATH only. Try to create 2 different
+XPATH locator if possible
+
+       */
+        //1. Open Chrome browser
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+       // 2. Go to https://practice.cydeo.com/forgot_password
+        driver.get("https://practice.cydeo.com/forgot_password ");
+
+      //  3. Locate all the WebElements on the page using XPATH and/or CSS
+     //   locator only (total of 6)
+
+
+    }
+}
