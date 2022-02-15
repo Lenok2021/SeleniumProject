4f91c86c998084ce62f89356962277bf2cc29ed7
cross <crosstown11223@yahoo.com>
2022-02-15 14:55:32 -0500

    TestNgIntro

--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNg.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNg.java
@@ -1,18 +1,36 @@
 package com.cydeo.tests.day5_testNG_intro_dropdowns;
 
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class TestNg {
 
+    @BeforeClass
+    public void setupMethod(){
+        System.out.println("---->BeforeClass is running!");
+    }
+
+    @AfterClass
+    public void tearDownClass(){
+        System.out.println("---->AfterClass is running!");
+    }
+
+    @BeforeMethod
+    public void setUpMethod(){
+        System.out.println("--->BeforeMethod is running!");
+    }
+
+    @AfterMethod
+    public void tearDownMethod(){
+        System.out.println("--->AfterMethod is running!");
+    }
+
     @Test
     public void test1(){
-        System.out.println("Test 1 is running ");
-
+        System.out.println("Test 1 is running...");
     }
+
     @Test
     public void test2(){
-        System.out.println("Test 2 is running");
+        System.out.println("Test 2 is running...");
     }
-
-
 }
