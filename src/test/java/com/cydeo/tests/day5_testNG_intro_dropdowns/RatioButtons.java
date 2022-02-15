d0b90348f539cdf1f8bd98af88d1ea41c31f5408
cross <crosstown11223@yahoo.com>
2022-02-15 12:28:51 -0500

    task RatioButtons completed

---
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/RatioButtons.java
@@ -0,0 +1,51 @@
+package com.cydeo.tests.day5_testNG_intro_dropdowns;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class RatioButtons {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        //1. Open Chrome browser
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        //2. Go to https://practice.cydeo.com/radio_buttons
+        driver.get("https://practice.cydeo.com/radio_buttons");
+
+        //3. Click to “Hockey” radio button
+        WebElement hockeyButton = driver.findElement(By.xpath("//input[@id='hockey']"));
+        Thread.sleep(1000);
+        hockeyButton.click();
+        System.out.println("hockeyButton.isSelected() = " + hockeyButton.isSelected());
+        System.out.println("hockeyButton.isDisplayed() = " + hockeyButton.isDisplayed());
+        System.out.println("hockeyButton.isEnabled() = " + hockeyButton.isEnabled());
+
+        System.out.println("=========================================================");
+        //  locate green button
+
+
+
+        driver.close();
+
+    }
+}
+
+
+/*
+driver.close?
+XPATH PRACTICES
+DO NOT USE ANY DEVELOPER TOOLS TO GET ANY LOCATORS.
+TC #2: Radiobutton handling
+1. Open Chrome browser
+2. Go to https://practice.cydeo.com/radio_buttons
+3. Click to “Hockey” radio button
+4. Verify “Hockey” radio button is selected after clicking.
+USE XPATH LOCATOR FOR ALL WEBELEMENT LOCATORS
+
+ */
