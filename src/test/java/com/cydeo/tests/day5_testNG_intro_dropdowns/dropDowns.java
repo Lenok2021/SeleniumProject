74784abf96dfc3bfd2a53570ddd18a5ce748f2b5
cross <crosstown11223@yahoo.com>
2022-02-18 08:33:18 -0500

    TestNgIntro

--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/dropDowns.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/dropDowns.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -26,6 +27,8 @@ public class dropDowns {
         //2. Go to https://practice.cydeo.com/dropdown
         driver.get("https://practice.cydeo.com/dropdown");
     }
+
+    
 
     @Test
     public void simpleDropdownTest() {
