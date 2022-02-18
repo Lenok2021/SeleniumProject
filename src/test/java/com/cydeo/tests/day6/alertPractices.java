c21d1e544caafd64b3df22bf5ce20574a76ff5c5
cross <crosstown11223@yahoo.com>
2022-02-18 14:28:31 -0500

    TestNgIntro

---
+++ src/test/java/com/cydeo/tests/day6/alertPractices.java
@@ -0,0 +1,76 @@
+package com.cydeo.tests.day6;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class alertPractices {
+
+    WebDriver  driver;
+
+
+    @BeforeMethod
+    public void setUpMethod(){
+        driver = WebDriverFactory.getDriver(" chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://practice.cydeo.com/javascript_alerts  ");
+
+
+
+    }
+
+    @Test
+    public void alert_test1(){
+
+
+      /*
+        TC #1: Information alert practice
+1. Open browser
+2. Go to website: http://practice.cydeo.com/javascript_alerts
+3. Click to “Click for JS Alert” button
+4. Click to OK button from the alert
+5. Verify “You successfully clicked an alert” text is displayed.
+       */
+
+    }
+    @Test
+    public void alert_test2(){
+
+
+        /*
+        TC #2: Confirmation alert practice
+1. Open browser
+2. Go to website: http://practice.cydeo.com/javascript_alerts
+3. Click to “Click for JS Confirm” button
+4. Click to OK button from the alert
+5. Verify “You clicked: Ok” text is displayed.
+
+
+         */
+
+    }
+
+    @Test
+    public void alert_test3(){
+
+
+        /*
+
+        TC #3: Information alert practice
+1. Open browser
+2. Go to website: http://practice.cydeo.com/javascript_alerts
+3. Click to “Click for JS Prompt” button
+4. Send “hello” text to alert
+5. Click to OK button from the alert
+6. Verify “You entered:  hello” text is displayed.
+
+         */
+
+    }
+
+
+}
