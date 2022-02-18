19d82da5abcdfe06aca7ceb4b837660f2b817ac8
cross <crosstown11223@yahoo.com>
2022-02-18 14:45:26 -0500

    TestNgIntro

--- src/test/java/com/cydeo/tests/day6/alertPractices.java
+++ src/test/java/com/cydeo/tests/day6/alertPractices.java
@@ -1,7 +1,11 @@
 package com.cydeo.tests.day6;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -9,23 +13,41 @@ import java.util.concurrent.TimeUnit;
 
 public class alertPractices {
 
-    WebDriver  driver;
+    WebDriver driver;
 
 
     @BeforeMethod
-    public void setUpMethod(){
-        driver = WebDriverFactory.getDriver(" chrome");
+    public void setUpMethod() {
+        driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("http://practice.cydeo.com/javascript_alerts  ");
 
 
-
     }
 
     @Test
-    public void alert_test1(){
+    public void alert_test1() {
+        //Click to “Click for JS Alert” button.
+        WebElement informationAlertButton = driver.findElement(By.xpath("//button[.='Click for JS Alert'] "));
+        informationAlertButton.click();
 
+        // Click to OK button from the alert.
+        //to be able to  click on Alert OK button  we need to switch driver's focus to Alert itself.
+        Alert alert = driver.switchTo().alert();
+
+        // Click to OK button from the alert.
+        alert.accept();
+
+        //Verify “You successfully clicked an alert” text is displayed.
+        WebElement resultText = driver.findElement(By.xpath("//p[@id = 'result'] "));
+        // verify  is displayed
+        Assert.assertTrue(resultText.isDisplayed(), "Result text is not displayed");
+
+        // verify  is the  Text  is  matching with "The text is not matching".
+        String actualText = resultText.getText();
+        String expectedText = "You successfully clicked an alert";
+        Assert.assertEquals(actualText,expectedText,"The text is not matching");
 
       /*
         TC #1: Information alert practice
@@ -37,10 +59,11 @@ public class alertPractices {
        */
 
     }
+
     @Test
-    public void alert_test2(){
+    public void alert_test2() {
 
-
+// //button[@onclick = 'jsConfirm()']
         /*
         TC #2: Confirmation alert practice
 1. Open browser
@@ -55,7 +78,7 @@ public class alertPractices {
     }
 
     @Test
-    public void alert_test3(){
+    public void alert_test3() {
 
 
         /*
