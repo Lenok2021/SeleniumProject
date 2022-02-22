a9f1f3aa600b174f256e347ab8b3f7d7f7858fa5
cross <crosstown11223@yahoo.com>
2022-02-22 16:20:12 -0500

    BrowserUtils class

--- src/test/java/com/cydeo/utilities/LoginScenario.java
+++ src/test/java/com/cydeo/utilities/LoginScenario.java
@@ -1,32 +1,64 @@
 package com.cydeo.utilities;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
 public class LoginScenario {
-    WebDriver driver;
+
+    public WebDriver driver;
 
     @BeforeMethod
-    public void setUp() {
-        WebDriverFactory.getDriver("chrome");
+    public void setupMethod(){
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
     }
 
+    //TC #3: Login scenario
     @Test
-    public void login_Scenario() {
-        //Go to : http://login1.nextbasecrm.com/
-        driver.get("http://login1.nextbasecrm.com/");
+    public void crm_login_test(){
 
-        //Enter valid username
+        //2. Go to : https://login1.nextbasecrm.com/
+        driver.get("https://login1.nextbasecrm.com/");
 
+        //3. Enter valid username
+        WebElement inputUsername = driver.findElement(By.xpath("//input[@name='USER_LOGIN']"));
+        inputUsername.sendKeys("helpdesk1@cybertekschool.com");
 
+        //helpdesk1@cybertekschool.com  UserUser
+        //Helpdesk2@cybertekschool.com  UserUser
+
+        //4. Enter valid password
+        WebElement inputPassword = driver.findElement(By.xpath("//input[@name='USER_PASSWORD']"));
+        inputPassword.sendKeys("UserUser");
+
+        //5. Click to Log In button
+        WebElement loginButton = driver.findElement(By.xpath("//input[@value='Log In']"));
+        loginButton.click();
+
+        //6. Verify title is as expected:
+        //Expected: Portal
+        BrowserUtils.verifyTitle(driver, "Portal");
 
     }
-    /*
-    TC #3: Login scenario
+
+    @AfterMethod
+    public void tearDown() {
+        driver.close();
+    }
+
+
+/*
+   TC #3: Login scenario
  1. Create new test and make set ups
 2. Go to : http://login1.nextbasecrm.com/
 3. Enter valid username
