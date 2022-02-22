84ca295dc71f669b66ac74bae534300822847b2c
cross <crosstown11223@yahoo.com>
2022-02-22 11:45:44 -0500

    window handling task

---
+++ src/test/java/com/cydeo/tests/day7/WindowHandle.java
@@ -0,0 +1,63 @@
+package com.cydeo.tests.day7;
+
+import com.cydeo.utilities.WebDriverFactory;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+public class WindowHandle {
+
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod() {
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+
+    @Test
+    public void window_handling_test() {
+        //2. Go to: https://www.amazon.com
+        driver.get("https://www.amazon.com");
+
+        //3. Copy-paste the lines from below into your class
+        ((JavascriptExecutor) driver).executeScript("window.open('https://google.com','_blank');");
+        ((JavascriptExecutor) driver).executeScript("window.open('https://etsy.com','_blank');");
+        ((JavascriptExecutor) driver).executeScript("window.open('https://facebook.com','_blank');");
+
+        //4. Create a logic to switch to the tab where Etsy.com is open
+        Set<String> allWindowHandles = driver.getWindowHandles();
+
+        for (String each : allWindowHandles) {
+
+            driver.switchTo().window(each);
+            System.out.println("Current URL " + driver.getCurrentUrl());
+            if (driver.getCurrentUrl().contains("Etsy")) {
+                break;
+            }
+        }
+        //5. Assert:Title contains “Etsy”
+
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "Etsy";
+
+        Assert.assertTrue(actualTitle.contains(expectedTitle));
+
+        // we want  to  create  situation  when the driver get (contains)  Etsy,
+        // driver  stop  on this  page  do not go forward
+        // do not  switch the page
+        // when  we print URL out   we do not see Google
+
+
+    }
+
+
+}
