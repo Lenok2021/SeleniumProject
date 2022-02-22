9eb3fc4719f9d49f626a6713be8b14b12df56543
cross <crosstown11223@yahoo.com>
2022-02-22 15:26:58 -0500

    BrowserUtils class

--- src/test/java/com/cydeo/utilities/BrowserUtils.java
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -1,4 +1,71 @@
 package com.cydeo.utilities;
+/*
+In this class only general utility methods
+that are not related to some specific page.
+That means I can you use this method whatever I want in projects
+ */
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+
+import java.util.Set;
 
 public class BrowserUtils {
+/*
+This method will accept int (int seconds) and execute Thread.sleep
+for given duration
+ */
+
+    public static void sleepMethod(int second) {
+        second *= 1000;
+        try {
+            Thread.sleep(second);
+        } catch (InterruptedException e) {
+
+        }
+    }
+
+    public static void switchWindowAndVerify(WebDriver driver, String expectedInUrl, String expectedTitle) {
+        Set<String> allWindowHandles = driver.getWindowHandles();
+
+        for (String each : allWindowHandles) {
+
+            driver.switchTo().window(each);
+
+            System.out.println("Current URL " + driver.getCurrentUrl());
+
+            if (driver.getCurrentUrl().contains(expectedInUrl)) {
+                break;
+            }
+        }
+
+        String actualTitle = driver.getTitle();
+
+        Assert.assertTrue(actualTitle.contains(expectedTitle));
+
+
+      /*
+        TC #2: Create utility method
+ 1. Create a new class called BrowserUtils
+2. Create a method to make Task1 logic re-usable
+3. When method is called, it should switch window and verify title.
+
+
+Method info:
+• Name: switchWindowAndVerify
+• Return type: void
+• Arg1: WebDriver
+• Arg2: String expectedInUrl
+• Arg3: String expectedTitle
+
+
+
+
+
+       */
+
+
+    }
+
+
 }
