17ceaf13a445d74ab3d0dd8b6f0c506e2dcb2f8b
cross <crosstown11223@yahoo.com>
2022-02-25 15:22:25 -0500

    BrowserUtils class

---
+++ src/test/java/com/cydeo/tests/day8_Properties/ReadingProperties.java
@@ -0,0 +1,15 @@
+package com.cydeo.tests.day8_Properties;
+
+import org.testng.annotations.Test;
+
+public class ReadingProperties {
+
+
+    @Test
+    public void reading_from_properties_test() {
+
+
+
+
+    }
+}
