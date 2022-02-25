39a596fe3fde187f3bc7e690a11c47a3c05edb6c
cross <crosstown11223@yahoo.com>
2022-02-25 16:38:19 -0500

    ConfigPractice_class

--- src/test/java/com/cydeo/tests/day8_Properties/T4_Config_Practice.java
+++ src/test/java/com/cydeo/tests/day8_Properties/T4_Config_Practice.java
@@ -1,9 +1,62 @@
 package com.cydeo.tests.day8_Properties;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
 public class T4_Config_Practice {
+WebDriver driver;
+    @BeforeMethod
+    public void setUp() {
+
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://google.com");
+
+    }
+
+    @Test
+    public void google_search_test() {
+
+        WebElement  googleSearch = driver.findElement(By.xpath("//input[@name = 'q']"));
+
+        googleSearch.sendKeys("apple" + Keys.ENTER);
+
+
+        String expectedTitle = " apple - Google Search";
+        String actualTitle = driver.getTitle();
+
+        Assert.assertEquals(actualTitle, expectedTitle);
 
 
 
-
-
+    }
 }
+
+/*
+
+TC #4: Google search
+1- Open a chrome browser
+2- Go to: https://google.com
+3- Write “apple” in search box
+4- Verify title:
+Expected: apple - Google Search
+
+
+Use `configuration.properties` for the following:
+1. The browser type
+2. The URL
+3. The search keyword
+4. Make title verification dynamic. If search value changes, title
+assertion should not fail.
+ */
