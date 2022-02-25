1ee86ecc98066156afb95c44db1d2b1fc1aae683
cross <crosstown11223@yahoo.com>
2022-02-25 15:08:00 -0500

    BrowserUtils class

--- src/test/java/com/cydeo/tests/day8/LearningProperties.java
+++ src/test/java/com/cydeo/tests/day8/LearningProperties.java
@@ -6,8 +6,15 @@ public class LearningProperties {
 
     @Test
     public void testName() {
+        //we provide (pass) key  we get value
+        // key = value
+        //os.name = Windows 10
+        // user.name = cross
+        //  you have a key open the door, The key has to be exactly the same to open the door
+        // if you don't provide the correct key, you don't get value.
 
         System.out.println("System.getProperty(\"os.name\") = " + System.getProperty("os.name"));
+        System.out.println("System.getProperty(\"user.name\") = " + System.getProperty("user.name"));
 
     }
 }
