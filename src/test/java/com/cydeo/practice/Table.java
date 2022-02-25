552ae50fa83c46003034234183cc3394df1d21f5
cross <crosstown11223@yahoo.com>
2022-02-24 20:13:22 -0500

    BrowserUtils class

--- src/test/java/com/cydeo/practice/Table.java
+++ src/test/java/com/cydeo/practice/Table.java
@@ -8,14 +8,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class Table {
-WebDriver driver;
+    WebDriver driver;
+
     @BeforeMethod
     public void setUp() {
         WebDriverManager.chromedriver().setup();
-        driver= new ChromeDriver();
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
@@ -27,7 +29,28 @@ WebDriver driver;
         driver.get("http://practice.cydeo.com/");
         WebElement table = driver.findElement(By.xpath("//div[@class = 'large-12 columns']//a[. = 'Sortable Data Tables']"));
         table.click();
+        // return the table itself
+        WebElement table1 = driver.findElement(By.xpath("//table[@id = 'table1']"));
+        // return all rows
+        List<WebElement> rows = driver.findElements(By.xpath("//table[@id = 'table1']//tr"));
+        for (WebElement each : rows) {
+            System.out.println(each.getText());
+        }
+        System.out.println("==================================================");
+        // return all rows in the body
+        List<WebElement> rowsInTheBody = driver.findElements(By.xpath("//table[@id = 'table1']//tbody//tr"));
+        for (WebElement each : rowsInTheBody) {
+            System.out.println(each.getText());
 
+        }
+        System.out.println("===================================================");
+        //I want to return 3 row   from the table
+        WebElement thirdRow = driver.findElement(By.xpath("//table[@id = 'table1']//tbody//tr[3]"));
+        System.out.println(thirdRow.getText());
+
+        System.out.println("=================================================");
+         //return forthRow
+        WebElement forthRow = driver.findElement(By.xpath());
 
 
     }
