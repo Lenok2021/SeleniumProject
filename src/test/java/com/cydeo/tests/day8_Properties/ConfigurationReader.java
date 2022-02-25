f114bfd8e30f783969597a82a01295a8f24516ea
cross <crosstown11223@yahoo.com>
2022-02-25 16:14:51 -0500

    BrowserUtils class

--- src/test/java/com/cydeo/tests/day8_Properties/ConfigurationReader.java
+++ src/test/java/com/cydeo/tests/day8_Properties/ConfigurationReader.java
@@ -1,8 +1,19 @@
 package com.cydeo.tests.day8_Properties;
 
+import java.io.FileInputStream;
+import java.util.Properties;
+
 public class ConfigurationReader {
 
+    // 1. - Create the Object of Properties.
+    private static Properties properties = new Properties();   //  we created Object from Java.util
 
+    static{
+
+
+
+
+    }
 
 
 
