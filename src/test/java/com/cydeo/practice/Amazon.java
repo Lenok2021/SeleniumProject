c91cd8d18adff021f016b13851366047f5b54cf6
cross <crosstown11223@yahoo.com>
2022-02-24 21:12:11 -0500

    BrowserUtils class

---
+++ src/test/java/com/cydeo/practice/Amazon.java
@@ -0,0 +1,52 @@
+package com.cydeo.practice;
+
+import com.cydeo.utilities.WebDriverFactory;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class Amazon {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://www.amazon.com/");
+    }
+
+    @Test
+    public void testName() throws InterruptedException {
+        Thread.sleep(1000);
+        WebElement sighIn = driver.findElement(By.xpath("//div[@class = 'nav-line-1-container']//span[@id = 'nav-link-accountList-nav-line-1']"));
+        sighIn.click();
+        Thread.sleep(1000);
+        WebElement login = driver.findElement(By.xpath("//input[@id = 'ap_email']"));
+        login.sendKeys("lena.morgacheva2006@mail.ru");
+        Thread.sleep(1000);
+        WebElement continueTab  = driver.findElement(By.id("continue"));
+        continueTab.click();
+        Thread.sleep(1000);
+        WebElement password = driver.findElement(By.id("ap_password"));
+        password.sendKeys("Lenok2021");
+        Thread.sleep(1000);
+        WebElement sighIn1 = driver.findElement(By.id("signInSubmit"));
+        sighIn1.click();
+        Thread.sleep(1000);
+        WebElement all = driver.findElement(By.xpath("//i[@class = 'hm-icon nav-sprite']"));
+        all.click();
+        
+
+
+
+    }
+}
