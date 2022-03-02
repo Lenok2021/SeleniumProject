2d734236dd5679468bf2bc667289af288b218f6c
cross <crosstown11223@yahoo.com>
2022-03-01 23:08:31 -0500

    FindElements

--- src/test/java/com/cydeo/homework/FindElements.java
+++ src/test/java/com/cydeo/homework/FindElements.java
@@ -1,9 +1,15 @@
 package com.cydeo.homework;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class FindElements {
 
@@ -12,19 +18,21 @@ public class FindElements {
     @BeforeMethod
     public void setUp() {
 
-        WebDriverFactory.getDriver("chrome");
-
-
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://www.openxcell.com ");
     }
 
     @Test
     public void checkingNumberOfLinks() {
 
 
+        //Count the number of the links on the page and verify
 
-
-
-
+        List<WebElement> links = driver.findElements(By.xpath("//a"));
+        int amount = links.size();
+        System.out.println("amount = " + amount);
 
      /*
        TC #1: Checking the number of links on the page
@@ -36,16 +44,55 @@ Expected: 332
       */
 
     }
-}
 
 
-/*
+    @Test
+    public void printingOutAllText_fromLinks() {
 
-TC #2: Printing out the texts of the links on the page
+        //Count the number of the links on the page and verify
+
+        List<WebElement> links = driver.findElements(By.xpath("//a"));
+
+        for (WebElement eachLink : links) {
+            try {
+                System.out.println(eachLink.getText());
+            } catch (StaleElementReferenceException ignored) {
+            }
+
+        }
+
+
+        /*
+        TC #2: Printing out the texts of the links on the page
 1. Open Chrome browser
 2. Go to https://www.openxcell.com
 3. Print out all of the texts of the links on the page
 
+        */
+
+    }
+
+    @Test
+    public void numberEmptyLinks() {
+
+        List<WebElement> links = driver.findElements(By.xpath("//a"));
+
+        int amountOfEmptyLinks = 0;
+
+        for (WebElement eachLink : links) {
+
+            try {
+                String nameOfLink = eachLink.getText();
+                if (nameOfLink.isEmpty()) {
+                    amountOfEmptyLinks++;
+                }
+            } catch (StaleElementReferenceException ignored) {
+            }
+        }
+
+        System.out.println("amountOfEmptyLinks = " + amountOfEmptyLinks);
+        /*
+       
 TC #3: Counting the number of links that does not have text
 1. Open Chrome browser
 2. Go to https://www.openxcell.com
@@ -57,3 +104,9 @@ FROM COMPUTER TO COMPUTER, OR FOR DIFFERENT OS (MAC-WINDOWS). SO
 IF YOU ARE GETTING A DIFFERENT NUMBER, JUST USE THAT NUMBER FOR
 VERIFICATION.
  */
+
+
+    }
+}
+
+
