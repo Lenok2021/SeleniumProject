e996f2de52d8a3d2bd2b3f12517e208995a3e63d
cross <crosstown11223@yahoo.com>
2022-03-01 23:09:14 -0500

    FindElements

--- src/test/java/com/cydeo/homework/FindElements.java
+++ src/test/java/com/cydeo/homework/FindElements.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -106,6 +107,11 @@ VERIFICATION.
  */
 
 
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.close();
     }
 }
 
