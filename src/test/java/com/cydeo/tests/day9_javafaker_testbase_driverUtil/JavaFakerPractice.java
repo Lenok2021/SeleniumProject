6dd4e2adfabc2f5fbb9d2fb44b9c5299620e53c0
cross <crosstown11223@yahoo.com>
2022-03-04 11:58:50 -0500

    UserStory5

--- src/test/java/com/cydeo/tests/day9_javafaker_testbase_driverUtil/JavaFakerPractice.java
+++ src/test/java/com/cydeo/tests/day9_javafaker_testbase_driverUtil/JavaFakerPractice.java
@@ -22,6 +22,23 @@ public class JavaFakerPractice {
         System.out.println(faker.name().lastName());
         System.out.println(faker.name().fullName());
 
+        System.out.println("=========================================================");
+
+        System.out.println("faker.numerify(\"###-###-####\") = " + faker.numerify("###-###-####"));
+
+        System.out.println("faker.numerify(\"312-###-####\") = " + faker.numerify("312-###-####"));
+
+        // ("312-###-####") =>  where  the   312  is unchangeable
+        //### is randomly-generated.
+
+        System.out.println("faker.numerify(\"###-100-####\") = " + faker.numerify("###-100-####"));
+
+        System.out.println("============================================================");
+
+        // we can generate name  as well
+        // will return  letter  where we passed ?
+        System.out.println("faker.letterify(\"????-???\") = " + faker.letterify("????-???"));
+        System.out.println("faker.letterify(\"??AA??BB\") = " + faker.letterify("??AA??BB"));
 
     }
 }
