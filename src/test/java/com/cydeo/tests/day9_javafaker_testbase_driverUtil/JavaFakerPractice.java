ffe480a686fb54d14560be07baff4f3580449d3a
cross <crosstown11223@yahoo.com>
2022-03-04 11:46:01 -0500

    UserStory5

---
+++ src/test/java/com/cydeo/tests/day9_javafaker_testbase_driverUtil/JavaFakerPractice.java
@@ -0,0 +1,26 @@
+package com.cydeo.tests.day9_javafaker_testbase_driverUtil;
+
+import com.github.javafaker.Faker;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class JavaFakerPractice {
+
+
+    @Test
+    public void tearDown() {
+        //1.  created Faker Object to reach methods
+        Faker faker = new Faker();
+
+        System.out.println(faker.name().firstName());
+        System.out.println(faker.name().lastName());
+        System.out.println(faker.name().fullName());
+
+    }
+}
