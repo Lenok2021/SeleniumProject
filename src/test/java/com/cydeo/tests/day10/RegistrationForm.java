fc1fa6136cf2914f85c43e54f3ec4771483fae8d
cross <crosstown11223@yahoo.com>
2022-03-08 12:20:26 -0500

    RegistrationForm.java

--- src/test/java/com/cydeo/tests/day10/RegistrationForm.java
+++ src/test/java/com/cydeo/tests/day10/RegistrationForm.java
@@ -2,6 +2,9 @@ package com.cydeo.tests.day10;
 
 import com.cydeo.utilities.ConfigurationReader;
 import com.cydeo.utilities.Driver;
+import com.github.javafaker.Faker;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class RegistrationForm {
@@ -15,6 +18,27 @@ public class RegistrationForm {
         //2. Go to website: https://practice.cydeo.com/registration_form
         Driver.getDriver().get(ConfigurationReader.getProperty("registration.form.url"));
 
+        //before  that I created Java Faker Class
+        Faker faker = new Faker();
+
+        //3. Enter first name
+        WebElement inputFirstName = Driver.getDriver().findElement(By.xpath("//input[@name = 'firstname']"));
+        inputFirstName.sendKeys(faker.name().firstName());
+
+        //4. Enter last name
+        WebElement inputLastName = Driver.getDriver().findElement(By.xpath("//input[@name = 'lastname']"));
+        inputLastName.sendKeys(faker.name().lastName());
+
+        //5. Enter username
+        WebElement inputUserName = Driver.getDriver().findElement(By.xpath("//input[@name = 'username']"));
+        inputUserName.sendKeys(faker.name().username());
+
+        //6. Enter email address
+        WebElement inputEmail = Driver.getDriver().findElement(By.xpath("//input[@name = 'email']"));
+        inputEmail.sendKeys(faker.name().username());
+
+        //7. Enter password
+        //8. Enter phone number
 
 
     }
