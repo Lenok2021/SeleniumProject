c1d1fed0e46e8ae8625813dc759724ac9085c09f
cross <crosstown11223@yahoo.com>
2022-03-08 11:18:15 -0500

    Singleton

---
+++ src/test/java/com/cydeo/homework/DataProvide.java
@@ -0,0 +1,26 @@
+package com.cydeo.homework;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class DataProvide {
+
+
+    @DataProvider(name = "data-provider")
+    public Object[][] dpMethod() {
+        return new Object[][]{{"First-Value"},
+                {"Second-Value"}};
+    }
+
+    @Test(dataProvider = "data-provider")
+    public void myTest(String val) {
+        System.out.println("Passed Parameter Is : " + val);
+    }
+}
+
+
+
+
+
+
+
