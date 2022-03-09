8200f0ef0359b181703d2478109b3118b7cc0754
cross <crosstown11223@yahoo.com>
2022-03-09 10:37:32 -0500

    RegistrationForm_JavaFaker.java

--- src/test/java/com/cydeo/homework/RightClick_DoubleClick_Actions_Class.java
+++ src/test/java/com/cydeo/homework/RightClick_DoubleClick_Actions_Class.java
@@ -6,6 +6,7 @@ import com.cydeo.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class RightClick_DoubleClick_Actions_Class {
@@ -37,18 +38,19 @@ public class RightClick_DoubleClick_Actions_Class {
     }
 
     @Test
-    public void testDoubleClick() {
+    public void testDoubleClick() throws InterruptedException {
 
         Driver.getDriver().get("https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_ondblclick");
-
         Driver.getDriver().switchTo().frame("iframeResult");
-
-        WebElement  element = Driver.getDriver().findElement(By.xpath("//p[text() = 'Double-click this paragraph to trigger a function.']"));
-
-
+        // Double-click this paragraph to trigger a function.
+        WebElement pElm = Driver.getDriver().findElement(By.xpath("//p[.='Double-click this paragraph to trigger a function.']"));
+        Thread.sleep(2000);
         Actions actions = new Actions(Driver.getDriver());
-
-        actions.doubleClick(element).perform();
+        actions.doubleClick(pElm).perform();
+        // assert new p element with Hello World text displayed
+        WebElement resultElm = Driver.getDriver().findElement(By.xpath("//p[.='Hello World']")) ;
+       Assert.assertTrue(resultElm.isDisplayed() );
+        Thread.sleep(2000);
 
 
 
