5e2aa94260a5be90d731aa36f8320f0bf27afb28
cross <crosstown11223@yahoo.com>
2022-03-14 12:33:28 -0400

    ExplicitWaitPractices

--- src/test/java/com/cydeo/tests/day13Recap/ExplicitWaitPractices.java
+++ src/test/java/com/cydeo/tests/day13Recap/ExplicitWaitPractices.java
@@ -2,36 +2,53 @@ package com.cydeo.tests.day13Recap;
 
 import com.cydeo.pages.DynamicControlPages;
 import com.cydeo.utilities.Driver;
-import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class ExplicitWaitPractices {
+    DynamicControlPages dynamicControlPages;
+
 
     @BeforeMethod
     public void setupMethod(){
 
         Driver.getDriver().get("https://practice.cydeo.com/dynamic_controls");
+
+
     }
+
 
     @Test
     public void remove_button_test() {
 
         // 3- Click to “Remove” button
-        DynamicControlPages dynamicControlPages = new DynamicControlPages();
-        Actions actions = new Actions(Driver.getDriver()) ;
+        dynamicControlPages = new DynamicControlPages();
 
-        actions.moveToElement(dynamicControlPages.button).click().perform();
+        dynamicControlPages.button.click();
 
         // 4- Wait until “loading bar disappears”
         WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
 
-
+        wait.until(ExpectedConditions.invisibilityOf(dynamicControlPages.loadingBar));
 
         // 5- Verify:
         // a. Checkbox is not displayed
+
+        try {
+            Assert.assertTrue(!(dynamicControlPages.checkbox.isDisplayed()));
+            Assert.assertFalse(dynamicControlPages.checkbox.isDisplayed());
+        }catch (NoSuchElementException n){
+            Assert.assertTrue(true);
+
+        }
         // b. “It’s gone!” message is displayed.
+
+        Boolean isDisplayed = dynamicControlPages.message.isDisplayed();
+        System.out.println("isDisplayed = " + isDisplayed);
 
     }
 
